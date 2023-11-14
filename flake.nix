b1690680f9ac17d66a060875fea5b8b2a7471c81
Manatsawin Hanmongkolchai <git@whs.in.th>
2023-11-14 22:43:58 +0700

    Add crisp-status-local

--- flake.nix
+++ flake.nix
@@ -1,6 +1,7 @@
 {
 	outputs = { self, nixpkgs, flake-utils }: {
 		nixosModules.readsb = import ./readsb/module.nix;
+		nixosModules.crisp-status-local = import ./crisp-status-local/module.nix;
 	} // flake-utils.lib.eachDefaultSystem (system: 
 		let
 			pkgs = import nixpkgs { inherit system; };
@@ -9,6 +10,7 @@
 		in {
 			packages = {
 				readsb = pkgs.callPackage ./readsb/package.nix {};
+				crisp-status-local = pkgs.callPackage ./crisp-status-local/package.nix {};
 			};
 		}
 	);
